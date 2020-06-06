8f4b4c66c5598eeaa3750adef15ca943bd4caa76
KanarekLife <kanareklife@gmail.com>
2020-06-06 17:06:32 +0200

    Basic header and routing

--- src/App.vue
+++ src/App.vue
@@ -1,29 +1,26 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App" />
-  </div>
+    <div id="app">
+        <Header/>
+        <router-view/>
+    </div>
 </template>
 
-<script lang="ts">
-import { Component, Vue } from "vue-property-decorator";
-import HelloWorld from "./components/HelloWorld.vue";
-
-@Component({
-  components: {
-    HelloWorld
-  }
-})
-export default class App extends Vue {}
-</script>
-
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+    html,
+    body {
+        margin: 0;
+        padding: 0;
+        height: 100%;
+    }
 </style>
+<script lang="ts">
+    import Vue from "vue";
+    import {Component} from "vue-property-decorator";
+    import Header from "@/components/Header.vue";
+
+    @Component({
+        components: {Header}
+    })
+    export default class App extends Vue {
+    }
+</script>
